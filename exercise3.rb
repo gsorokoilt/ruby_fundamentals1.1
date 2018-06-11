e9348946716910584dcc1853b73731a52a1b41af
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-06-11 15:26:50 -0400

    data types and operators

--- exercise3.rb
+++ exercise3.rb
@@ -1,5 +1,12 @@
-puts "What is your name?"
+# puts "What is your name?"
+#
+# name = gets.chomp
+#
+# puts "Hello #{name}, nice to meet you"
 
-name = gets.chomp
-
-puts "Hello #{name}, nice to meet you"
+# counter = 10
+#
+# while counter < 20
+#   puts "counter is at #{counter}"
+#   counter += 5
+# end
