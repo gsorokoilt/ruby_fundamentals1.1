41c72741f1023021e43110ba189de085d1842bc4
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-06-11 16:23:40 -0400

    add go home option

--- exercise3.rb
+++ exercise3.rb
@@ -10,3 +10,20 @@
 #   puts "counter is at #{counter}"
 #   counter += 5
 # end
+
+distance_from_home = 0
+choice = ""
+
+while distance_from_home >= 0 && choice != "go home"
+  puts "Would you like to walk or run?"
+  choice = gets.chomp
+    if choice == "walk"
+      distance_from_home += 1
+    elsif choice == "run"
+      distance_from_home += 5
+    elsif choice == "go home"
+      puts "bye bye!"
+    else puts "invalid response"
+    end
+   puts "You are now #{distance_from_home}km from home"
+end
