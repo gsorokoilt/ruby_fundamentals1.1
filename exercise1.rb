afe3be6869596feb99de2fe43c3cfa1fb44fa5ea
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-06-11 14:51:25 -0400

    complete exercises

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,11 @@
-p 2
-p 3
-p 2 + 3
+# p 2
+# p 3
+# p 2 + 3
+#
+# p 'Betty\'s Pie Shop'
+#
+# p "Ada Lovelace lived for #{1989-1904} years"
+
+puts "Hello\t\world"
+
+puts "Hello\b\b\b\b\bGoodbye world"
---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+
+# def tip(cost)
+#   cost * 1.20
+# end
+#
+# p tip(130)
+
+# p "I'm number " + 1.to_s
+
+# p "The product of 45628 and 7839 is #{45628*7839}"
+
+p (10 < 20 && 30 < 20) || !(10 == 11)
