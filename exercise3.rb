1ce94d253f0bde4cc309d92484faa8c704ec7c8b
GORDON SOROKOLIT <gordon.sorokolit@gmail.com>
2018-06-11 18:52:34 -0400

    complete energy meter

--- exercise3.rb
+++ exercise3.rb
@@ -11,19 +11,32 @@
 #   counter += 5
 # end
 
+
+
+energy = 3
 distance_from_home = 0
 choice = ""
+
+
 
 while distance_from_home >= 0 && choice != "go home"
   puts "Would you like to walk or run?"
   choice = gets.chomp
     if choice == "walk"
       distance_from_home += 1
-    elsif choice == "run"
+      energy += 3
+    elsif choice == "run" && energy >=0
       distance_from_home += 5
+      energy -=10
     elsif choice == "go home"
       puts "bye bye!"
     else puts "invalid response"
-    end
-   puts "You are now #{distance_from_home}km from home"
+  end
+
+  if energy<=0 && choice == "run"
+    puts "not enough energy"
+    distance_from_home += 0
+  end
+
+   puts "You are now #{distance_from_home}km from home, energy #{energy}"
 end
